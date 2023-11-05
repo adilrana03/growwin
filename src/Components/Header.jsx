8ac3c56d4d7f19603d03d4b05a24688b94797177
adilrana03 <adilrana2014.com>
2023-11-05 13:34:02 +0530

    added hamberber in mobile view

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,109 +1,99 @@
-import React from 'react';
-// import logo from '../assets/logo.jpeg'
-// import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import appstore from '../assets/apple2.png';
 import google from '../assets/game.png'
-import appstore from '../assets/app-store.png'
-import home from '../assets/home.png'
-import group from '../assets/group.png'
-import cart from '../assets/shopping-cart.png'
-import globe from '../assets/globe.png'
+
 function Header() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <>
-            <div className="flex justify-around py-2 pt-5 top-0 bg-white  fixed w-full z-20">
-                {/* Logo and Brand Name */}
-                <a href="/" className="flex gap-3 md:items-center ">
-                    {/* Placeholder for Logo if you wish to add it later */}
-                    {/* <img src={logo} alt="logo" className='h-10 w-10 rounded-full shadow-lg shadow-blue-400' /> */}
-                    <span className="text-red-500 text-xl font-bold ">Growwin</span>
-                </a>
+            <nav className="bg-white px-4 py-4 shadow-md fixed md:flex justify-around inset-x-0 top-0 z-20">
+                <div className="flex justify-between items-center">
+                    <a href="/" className="text-red-500 text-xl font-bold">Growwin</a>
 
-                {/* Navigation Links */}
-                <div className="space-x-8 hidden md:flex relative">
-                    {/* Trade Dropdown */}
-                    <div className="group">
-                        <a href="#" className="text-gray-600 hover:text-red-500">Trade</a>
-                        <div className="hidden group-hover:block absolute bg-white shadow-lg p-8 w-[300px] rounded-lg mt-1">
-                            <ul>
-                                <li><a href="#" className="hover:text-red-500  ">Forex Trading</a></li>
-                                <li><a href="#" className="hover:text-red-500 ">Commodities</a></li>
-                                <li><a href="#" className="hover:text-red-500 ">Stock Indices</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    {/* Markets Dropdown */}
-                    <div className="group">
-                        <a href="#" className="text-gray-600 hover:text-red-500">Markets</a>
-                        <div className="hidden group-hover:block absolute bg-white shadow-lg p-8 w-[300px] rounded-lg mt-1">
-                            <ul>
-                                <li><a href="#" className="hover:text-red-500">Cryptocurrency</a></li>
-                                <li><a href="#" className="hover:text-red-500">Equities</a></li>
-                                <li><a href="#" className="hover:text-red-500">Bonds</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    {/* About Us Dropdown */}
-                    <div className="group">
-                        <a href="#" className="text-gray-600 hover:text-red-500">About Us</a>
-                        <div className="hidden group-hover:block absolute bg-white shadow-lg p-8 rounded-lg w-[300px] mt-1">
-                            <ul>
-                                <li><a href="#" className="hover:text-red-500">Our Story</a></li>
-                                <li><a href="#" className="hover:text-red-500">Our Team</a></li>
-                                <li><a href="#" className="hover:text-red-500">Career Opportunities</a></li>
-                            </ul>
-                        </div>
+                    {/* Mobile Menu Button */}
+                    <div className="md:hidden">
+                        <button
+                            onClick={toggleMobileMenu}
+                            type="button"
+                            className="text-gray-700 hover:text-gray-900 focus:outline-none"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            {/* Icon for menu open/close */}
+                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                {isMobileMenuOpen ? (
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                ) : (
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                                )}
+                            </svg>
+                        </button>
                     </div>
                 </div>
 
-                {/* Social Media Icons */}
-                <div className="space-x-8 md:flex hidden">
-                    {/* Placeholder for Instagram Icon */}
-                    <a href="#">
-                        <div className="rounded-2xl">
-                            <img src={google} to your instagram icon className="h-8 w-8" alt="Instagram" />
-                        </div>
-                    </a>
-                    {/* Placeholder for App Store Icon */}
-                    <a href="#">
-                        <div className="rounded-2xl">
-                            <img src={appstore} to your app store icon className="h-8 w-8" alt="App Store" />
-                        </div>
-                    </a>
+                {/* Mobile Menu */}
+                <div className={`${isMobileMenuOpen ? 'block' : 'hidden'} md:hidden h-[300px] mt-1  `}>
+                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl font-light">Trade</a>
+                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl font-light">Markets</a>
+                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl font-light">About Us</a>
                 </div>
-            </div>
 
-            <div className="md:hidden fixed bottom-0 w-full bg-white z-30">
-                <nav className="flex justify-around items-center p-4">
-               
-                    <a href="#about" className="flex flex-col items-center">
-                        <img src={group} className='h-6 w-6' alt="" />
+                {/* Desktop Menu */}
+                <div className="hidden md:flex md:items-center md:justify-center">
+                    <div className="group px-5">
+                        <a href="#" className="text-gray-600 hover:text-red-500 px-3 rounded-md">Trade</a>
+                        <div className="hidden group-hover:block absolute mt-1">
+                            {/* Dropdown content */}
+                            <div className="bg-white shadow-lg rounded-lg p-5">
+                                <a href="#" className="hover:text-red-500 block">Forex Trading</a>
+                                <a href="#" className="hover:text-red-500 block">Commodities</a>
+                                <a href="#" className="hover:text-red-500 block">Stock Indices</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="group px-5">
+                        <a href="#" className="text-gray-600 hover:text-red-500 px-3 py-2 rounded-md">Markets</a>
+                        <div className="hidden group-hover:block absolute mt-1">
+                            {/* Dropdown content */}
+                            <div className="bg-white shadow-lg rounded-lg p-5">
+                                <a href="#" className="hover:text-red-500 block">Cryptocurrency</a>
+                                <a href="#" className="hover:text-red-500 block">Equities</a>
+                                <a href="#" className="hover:text-red-500 block">Bonds</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="group px-5">
+                        <a href="#" className="text-gray-600 hover:text-red-500 px-3 py-2 rounded-md">About Us</a>
+                        <div className="hidden group-hover:block absolute mt-1">
+                            {/* Dropdown content */}
+                            <div className="bg-white shadow-lg rounded-lg p-5">
+                                <a href="#" className="hover:text-red-500 block">Our Story</a>
+                                <a href="#" className="hover:text-red-500 block">Our Team</a>
+                                <a href="#" className="hover:text-red-500 block">Career Opportunities</a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <a href="#">
+                        <img src={appstore} className="h-8 w-8 ml-5" alt="App Store" />
+                    </a> */}
+                </div>
+                <div className='flex gap-4'>
+                    <button className=''><img className='h-8 w-8' src={appstore} alt="" /></button>
+                    <button className=''><img className='h-7 w-7' src={google} alt="" /></button>                    
+                </div>
+            </nav>
 
-                        <span className="text-xs mt-1">About</span>
-                    </a>
-
-                    {/* Markets Icon */}
-                    <a href="#markets" className="flex flex-col items-center">
-                        <img src={globe} className='h-6 w-6' alt="" />
-
-                        <span className="text-xs mt-1">Markets</span>
-                    </a>
-
-                    {/* Forex Icon */}
-                    <a href="#forex" className="flex flex-col items-center">
-                        <img src={cart} className='h-6 w-6' alt="" />
-                        <span className="text-xs mt-1">Forex</span>
-                    </a>
-                    <a href="/" className="flex flex-col items-center">
-                        <img src={home} className='h-6 w-6' alt="" />
-
-                        <span className="text-xs mt-1">Home</span>
-                    </a>
-                </nav>
-            </div>
-
+            {/* Backdrop - show only when mobile menu is open */}
+            {isMobileMenuOpen && (
+                <div
+                    className="bg-black bg-opacity-50 fixed inset-0 z-10 md:hidden transition-all delay-500 ease-out"
+                    onClick={toggleMobileMenu}
+                ></div>
+            )}
         </>
     );
 }
