66986f965958ebe0ada925ff4a39b86c7a34aea4
adilrana03 <adilrana2014.com>
2023-11-06 18:51:07 +0530

    final commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,38 @@
 import React, { useState } from 'react';
 import appstore from '../assets/app-store2.png';
 import google from '../assets/playstore2.png'
+import { RiArrowUpSLine } from 'react-icons/ri'
+import { HiMiniChevronDown } from 'react-icons/hi2'
+
+
 
 function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isTradeDropdownOpen, setIsTradeDropdownOpen] = useState(false);
+    const [isAboutUsDropdownOpen, setIsAboutUsDropdownOpen] = useState(false);
+    const [isMarketsDropdownOpen, setIsMarketsDropdownOpen] = useState(false);
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
+        // Close all dropdowns when the mobile menu is toggled
+        if (isTradeDropdownOpen) setIsTradeDropdownOpen(false);
+        if (isAboutUsDropdownOpen) setIsAboutUsDropdownOpen(false);
+        if (isMarketsDropdownOpen) setIsMarketsDropdownOpen(false);
+    };
+
+    const toggleTradeDropdown = (event) => {
+        event.stopPropagation();
+        setIsTradeDropdownOpen(!isTradeDropdownOpen);
+    };
+
+    const toggleAboutUsDropdown = (event) => {
+        event.stopPropagation();
+        setIsAboutUsDropdownOpen(!isAboutUsDropdownOpen);
+    };
+
+    const toggleMarketsDropdown = (event) => {
+        event.stopPropagation();
+        setIsMarketsDropdownOpen(!isMarketsDropdownOpen);
     };
 
     return (
@@ -36,22 +62,86 @@ function Header() {
                 </div>
 
                 {/* Mobile Menu */}
-                <div className={`${isMobileMenuOpen ? 'block' : 'hidden'} md:hidden h-[300px] mt-1  `}>
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl font-light">Trade</a>
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl font-light">Markets</a>
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl font-light">About Us</a>
-                    <div className='mt-4 px-4 w-full'>
-                        <a href=""> <button className='block my-3'><img className='h-8 bg-red-100' src={appstore} alt="" /></button></a>
-                        <a href=""> <button className=''><img className='h-8 bg-red-100' src={google} alt="" /></button></a>
+                <div className={`${isMobileMenuOpen ? 'block' : 'hidden'} md:hidden h-auto mt-1 bg-white  rounded-lg`}>
+                    <div className="px-4 py-2 text-xl font-light">
+                        {/* Trade Dropdown */}
+                        <button
+                            onClick={toggleTradeDropdown}
+                            className="text-gray-700 hover:bg-gray-200 w-full text-left flex justify-between items-center rounded-lg px-2 py-2 my-1 font-semibold"
+                        >
+                            Trade
+                            <span>
+                                {isTradeDropdownOpen ? (
+                                    <RiArrowUpSLine />
+                                ) : (
+                                        <HiMiniChevronDown />
+                                )}
+                            </span>
+                        </button>
+                        <div className={`${isTradeDropdownOpen ? 'block' : 'hidden'} bg-gray-50 rounded-lg  mb-2 p-3`}>
+                            <h1 className=''>Trade type</h1>
+                            <a href="#" className="block text-gray-700 hover:bg-gray-200 rounded-lg px-2 py-2 my-1 text-base">CFDs</a>
+                            <a href="#" className="block text-gray-700 hover:bg-gray-200 rounded-lg px-2 py-2 my-1 text-base">Options</a>
+                            <a href="#" className="block text-gray-700 hover:bg-gray-200 rounded-lg px-2 py-2 my-1 text-base">Multipliers</a>
+                        </div>
+
+                        {/* About Us Section */}
+                        <button
+                            onClick={toggleAboutUsDropdown}
+                            className="text-gray-700 hover:bg-gray-200 w-full text-left flex justify-between items-center rounded-lg px-2 py-2 my-1 font-semibold"
+                        >
+                            About Us
+                            {/* Icon logic for About Us */}
+                            <span>
+                                {isAboutUsDropdownOpen ? (
+                                    <RiArrowUpSLine />
+                                ) : (
+                                        <HiMiniChevronDown />
+                                )}
+                            </span>
+                        </button>
+                        <div className={`${isAboutUsDropdownOpen ? 'block' : 'hidden'} bg-gray-50 rounded-lg text-sm mb-2`}>
+                            {/* Content for About Us */}
+                            <p className="text-gray-600 px-2 py-2 my-1 text-base">
+                                We're a team dedicated to delivering the best trading experience. Learn more about our journey, values, and the people behind our company.
+                            </p>
+                        </div>
+
+                        {/* Markets Section */}
+                        <button
+                            onClick={toggleMarketsDropdown}
+                            className="text-gray-700 hover:bg-gray-200 w-full text-left flex justify-between items-center rounded-lg px-2 py-2 my-1 font-semibold"
+                        >
+                            Markets
+                            {/* Icon logic for Markets */}
+                            <span>
+                                {isMarketsDropdownOpen ? (
+                                    <RiArrowUpSLine />
+                                ) : (
+                                        <HiMiniChevronDown />
+                                )}
+                            </span>
+                        </button>
+                        <div className={`${isMarketsDropdownOpen ? 'block' : 'hidden'} bg-gray-50 rounded-lg mb-2 text-base`}>
+                            {/* Content for Markets */}
+                            <a href="#" className="block text-gray-700 hover:bg-gray-200 rounded-lg px-2 py-2 my-1">Forex</a>
+                            <a href="#" className="block text-gray-700 hover:bg-gray-200 rounded-lg px-2 py-2 my-1">Stocks</a>
+                            <a href="#" className="block text-gray-700 hover:bg-gray-200 rounded-lg px-2 py-2 my-1">Commodities</a>
+                        </div>
+
+                        {/* I  C O N S    H E  R   E */}
+                        <div className='block mt-3'>
+                            <a href="" className='block ml-2 mb-2'><button><img className='w-20 ' src={appstore} alt="" /></button></a>
+                            <a href="" className='block ml-2 '><button><img className='w-20 ' src={google} alt="" /></button></a>
+                        </div>
+
                     </div>
-
-
                 </div>
 
                 {/* Desktop Menu */}
                 <div className="hidden md:flex md:items-center md:justify-center">
                     <div className="group px-5">
-                        <a href="#" className="text-gray-600 hover:text-red-500 px-3 rounded-md">Trade</a>
+                        <a href="#" className="text-gray-600 hover:text-red-500 px-3 rounded-md ">Trade</a>
                         <div className="hidden group-hover:block absolute mt-1">
                             {/* Dropdown content */}
                             <div className="bg-white shadow-lg rounded-lg p-5">
